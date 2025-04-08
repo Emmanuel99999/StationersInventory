b5c05d122b2a2d688e7913c2e0d165e1d562a293
canbeagle <eafcanbeagle@gmail.com>
2025-04-07 22:32:58 -0500

    carpeta original eliminada, habían 2 proyectos, se trabajará solo con gestioninventario_mvc por que tiene más avance

--- GestionInventario_MVC/GestionInventario_MVC/Controllers/HomeController.cs
+++ GestionInventario_MVC/GestionInventario_MVC/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace GestionInventario_MVC.Controllers
             return View();
         }
 
+        public IActionResult Inventory()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- GestionInventario_MVC/GestionInventario_MVC/obj/Debug/net8.0/GestionInventario_MVC.AssemblyInfo.cs
+++ GestionInventario_MVC/GestionInventario_MVC/obj/Debug/net8.0/GestionInventario_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4fbe501ccbb6c08aaec9cd48232abf39b290cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
