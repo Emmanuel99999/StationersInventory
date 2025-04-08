b682fdc8283899bf5caedbbaff84bbc02a5ca0b5
Jacobo Loaiza Cano <akaload158@gmail.com>
2025-04-08 17:08:35 -0500

    Add files via upload

---
+++ Pages/_Host.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestionInventario_MVC.Pages
+{
+    public class _HostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
