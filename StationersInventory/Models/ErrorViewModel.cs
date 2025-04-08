9f6a016a5d191563403fb009b2bd79bdf032df39
Jacobo Loaiza Cano <akaload158@gmail.com>
2025-04-08 18:22:39 -0500

    Delete StationersInventory directory

--- StationersInventory/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StationersInventory.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
