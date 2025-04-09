534768b8b71e59eea092637fe7582ab679fe5a5c
canbeagle <eafcanbeagle@gmail.com>
2025-04-08 23:17:05 -0500

    added frontend design to the project

--- GestionInventario_MVC/GestionInventario_MVC/obj/Debug/net8.0/GestionInventario_MVC.AssemblyInfo.cs
+++ GestionInventario_MVC/GestionInventario_MVC/obj/Debug/net8.0/GestionInventario_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a829af85118629e4fe0d79ad394594b55631a1a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8664e9b49a2b196d1dab09fac74bb6a38d64288")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
