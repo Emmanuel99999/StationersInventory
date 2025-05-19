56a11ba603645cce6dab711bfd0daede9de9869f
canbeagle <eafcanbeagle@gmail.com>
2025-05-19 18:42:56 -0500

    añado primeros pasos para login

--- GestionInventario_MVC/GestionInventario_MVC/obj/Debug/net8.0/GestionInventario_MVC.AssemblyInfo.cs
+++ GestionInventario_MVC/GestionInventario_MVC/obj/Debug/net8.0/GestionInventario_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2e29a6516ab205ede11bc58084e36351b8c10c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19e20caab4e67ce3df18f915e86589e4a3bbd6ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
