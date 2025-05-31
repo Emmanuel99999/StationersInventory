9cf66844744a66e4586f2a4e149e1a6ded38deed
canbeagle <eafcanbeagle@gmail.com>
2025-05-31 01:39:03 -0500

    estable pero se bloquea

--- GestionInventario_MVC/GestionInventario_MVC/obj/Debug/net8.0/GestionInventario_MVC.AssemblyInfo.cs
+++ GestionInventario_MVC/GestionInventario_MVC/obj/Debug/net8.0/GestionInventario_MVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b3b8154c43713a681e2993709f06d7332283d83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daea2d0c14cd59e7a1074306914b186bc93d29b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionInventario_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
